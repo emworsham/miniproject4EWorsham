d850fe92487f5b04785adeeb42d5a7cfa4082ec2
eworsham <edworsham@mail.fhsu.edu>
2023-12-10 20:27:42 -0600

    Fixed logout redirection and logout button

--- quiz/urls.py
+++ quiz/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),  # The main page of your quiz app
     path('register/', views.register, name='register'),  # For user registration
     path('login/', views.login, name='login'),  # For user login
-    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('quiz/', views.take_quiz, name='take_quiz'),  # To take a quiz
     path('results/', views.quiz_results, name='quiz_results'),  # To view quiz results
     # Add other URL patterns here as needed
